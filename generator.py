d0519299221d8e476329609c107395ed2cffba76
Bogdan <flomaster3008@gmail.com>
2023-10-27 12:20:39 +0300

    dataset prepaired for usage

---
+++ generator.py
@@ -0,0 +1,32 @@
+'''
+Generator file for CycleGAN
+'''
+import torch
+import torch.nn as nn
+
+class ConvBlock(nn.Modul):
+    def __init__(self):
+        super().__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, padding_mode="reflect", **kwargs)
+            if down
+            else nn.ConvTranspose2d(in_channels, out_channels, **kwargs),
+            nn.InstanceNorm2d(out_channels),
+            nn.ReLU(inplace=True) if use_act else nn.Identity(),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.block = nn.Sequential(
+            ConvBlock(channels, channels, kernel_size=3, padding=1),
+            ConvBlock(channels, channels, use_act=False, kernel_size=3, padding=1),
+        )
+
+    def forward(self, x):
+        return x + self.block(x)
