2dcbf5c3f7e675008cc7da1dfb6e2145f79c4ebc
Bogdan <flomaster3008@gmail.com>
2023-11-08 19:14:43 +0300

    train.py comlete and bug fix

---
+++ discriminator.py
@@ -0,0 +1,110 @@
+'''
+Discriminator file for CycleGAN
+
+The image size should be 3x256x256.
+In this scenario, the Discriminator returns a 30x30 map of probabilities
+'''
+import torch
+import torch.nn as nn
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, norm_type = nn.InstanceNorm2d, use_activation=True, **kwargs):
+        super().__init__()
+
+        moduls = [
+            nn.Conv2d(in_channels, out_channels, kernel_size=4, padding= 1, bias = True, padding_mode="reflect", **kwargs),
+            norm_type(out_channels)]
+        
+        if use_activation:
+            moduls.append(nn.LeakyReLU(0.2, inplace=True))
+
+        self.conv = nn.Sequential(*moduls)
+
+    def forward(self, x):
+        return self.conv(x)
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels = 3, n_features = [64, 128, 256, 512], use_sigmoid=True):
+        super().__init__()
+
+        self.init_block = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels,
+                      out_channels= n_features[0],
+                      kernel_size=4,
+                      stride=2,
+                      padding=1,
+                      padding_mode='reflect'
+                      ),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+
+        middle_layers = []
+        #iterating for pairs of i and i+1 elements except the last one
+        for in_ch, out_ch in zip(n_features[:-2], n_features[1:-1]):
+            middle_layers.append(
+                ConvBlock(in_channels=in_ch,
+                          out_channels=out_ch,
+                          stride = 2)
+            )
+        middle_layers.append(
+            ConvBlock(in_channels=n_features[-2],
+                      out_channels=n_features[-1],
+                      stride = 1)
+        )
+        self.middle_block = nn.Sequential(*middle_layers)
+
+        last_block_list = []
+        last_block_list.append(
+            nn.Conv2d(
+                in_channels=n_features[-1],
+                out_channels=1,
+                kernel_size=4,
+                stride=1,
+                padding=1,
+                padding_mode='reflect'
+            )
+        )
+        if use_sigmoid:
+            last_block_list.append(nn.Sigmoid())
+        
+        self.last_block = nn.Sequential(*last_block_list)
+
+    def forward(self, x):
+        x = self.init_block(x)
+        x = self.middle_block(x)
+        return self.last_block(x)
+
+def test_shapes():
+    '''
+    print shape of intput after every layer block
+    '''
+    discr = Discriminator()
+    noise = torch.randn(1, 3, 256, 256)
+    names_and_shapes = {'input': noise.shape}
+
+    noise = discr.init_block(noise)
+    names_and_shapes['after initial block'] = noise.shape
+    
+    noise = discr.middle_block(noise)
+    names_and_shapes['after middle block'] = noise.shape
+
+    noise = discr.last_block(noise)
+    names_and_shapes['after last block'] = noise.shape
+
+    output_str_length = 20
+    for name, shape in names_and_shapes.items():
+        print(name.ljust(output_str_length), shape)
+    
+    #Check for output size is not required to allow for random input image sizes
+
+if __name__ == '__main__':
+    print('start testing discriminator')
+
+    print('-'*20)
+    test_shapes()
+    print('-'*20)
+
+    print('discriminator successfuly passed all tests')
+    
+    
+
---
+++ summer_winter_dataset.py
@@ -0,0 +1,160 @@
+from typing import Any
+from PIL import Image
+import os
+from torch.utils.data import Dataset
+from torchvision import transforms
+import matplotlib.pyplot as plt
+
+class SummerWinterDataset(Dataset):
+    def __init__(self, summer_path, winter_path, image_size = 256, transform = None):
+        self.summer_path = summer_path
+        self.winter_path = winter_path
+
+        self.image_size = image_size
+
+        self.summer_imgs_names = os.listdir(summer_path)
+        self.winter_imgs_names = os.listdir(winter_path)
+
+        self.len = max(len(self.summer_imgs_names), len(self.winter_imgs_names))
+        self.summer_len = len(self.summer_imgs_names)
+        self.winter_len = len(self.winter_imgs_names)
+
+        if transform is None:
+            transform = transforms.Compose([
+                            transforms.ToTensor(),
+                            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5],), 
+                            transforms.Resize(self.image_size, antialias=True)
+                        ])
+        self.transform = transform
+
+    def __len__(self):
+        return self.len
+    
+    def __getitem__(self, indx):
+        summer_img_name = self.summer_imgs_names[indx % self.summer_len]
+        winter_img_name = self.winter_imgs_names[indx % self.winter_len]
+
+        summer_img_full_path =  os.path.join(self.summer_path, summer_img_name)
+        winter_img_full_path =  os.path.join(self.winter_path, winter_img_name)
+
+        summer_img = self.transform(Image.open(summer_img_full_path))
+        winter_img = self.transform(Image.open(winter_img_full_path))
+
+        return summer_img, winter_img
+    
+    def param_info(self):
+        info_str = (f'path to summer images: {self.summer_path}\n'+
+            f'path to winter images: {self.winter_path}\n'
+            f'transform {str(self.transform)}\n' +
+            f'length of summer images: {self.summer_len}\n' + 
+            f'length of winter images: {self.winter_len}')
+        print(info_str)
+
+
+
+# эти тесты вышлядят некрасиво, мне не нравится 
+class ContitionalPrint:
+    def __init__(self, dont_print):
+        self.dont_print = dont_print
+    def __call__(self, *args, **kwds):
+        if not self.dont_print:
+            print(*args, **kwds)
+
+
+def test_1(summer_path, winter_path, silent_mode = False):
+    '''
+    testing simple iteration
+    with default path:
+        summer_path = 'data/test_summer'
+        winter_path = 'data/test_winter'
+    '''
+    
+    cprint = ContitionalPrint(dont_print=silent_mode)
+
+    # craeting Dataset
+    try:
+        
+        sw_ds = SummerWinterDataset(summer_path=summer_path,
+                                    winter_path=winter_path,
+                                    image_size = 256,
+                                    transform = None)
+    except Exception as e:
+        print('Unable to create SummerWinterDataset')
+        #it seems usles if there is an exception we will able to detect error place by traising
+        raise
+
+    # testing __len__ and __getitem__
+    cprint('-'*10)
+    cprint('call __len__:\n', len(sw_ds))
+    cprint('-'*10)
+    cprint('call shape of __getitem__ and indx=0:\n', sw_ds[0][0].shape, sw_ds[0][1].shape)
+    cprint('-'*10)
+    # check param_info
+    try:
+        sw_ds.param_info()
+    except Exception as e:
+        print('Exception from "SummerWinterDataset" from "param_info" method')
+        raise
+        
+    cprint('-'*10)
+    try:
+        for i, (img1, img2) in enumerate(sw_ds):
+            cprint(f"{i}/{len(sw_ds)}", end='\r')
+        cprint('iteration test - success')
+    except Exception as e:
+        print('Unable to iterate')
+        # it seems lb usless again
+        raise
+
+def test_2(summer_path, winter_path , n=5,):
+    '''
+    show n first images from dataset
+    defoul n = 5
+    '''
+
+    # if suddenly test_1 wasnt called  
+    try:
+        sw_ds = SummerWinterDataset(summer_path=summer_path,
+                                    winter_path=winter_path,
+                                    image_size = 256,
+                                    transform = None)
+    except Exception as e:
+        print('Unable to create SummerWinterDataset')
+        #it seems usles if there is an exception we will able to detect error place by traising
+        raise
+    
+    # show 2xn grid of images
+    #first line - summer imgs, and second winter
+    plt.figure(figsize=(12, 6))
+    for i, (summer_img, winter_img) in enumerate(sw_ds):
+        if i >= 5:
+            break
+
+        #I assume that that image mean and std both equals 0.5
+        mean = std = 0.5
+        summer_img = (summer_img  * std) + mean
+        winter_img = (winter_img  * std) + mean
+
+
+        plt.subplot(2, n, i+1)
+        plt.axis("off")
+        plt.imshow(summer_img.permute(1, 2, 0))
+        plt.title('Summer in Yosemite')
+
+        plt.subplot(2, n, i+6)
+        plt.axis("off")
+        plt.imshow(winter_img.permute(1, 2, 0))
+        plt.title('Winter in Yosemite')
+    plt.show()
+
+if __name__ == '__main__':
+    print('start testing summer_winter_dataset')
+
+    current_directory = os.getcwd()
+    summer_path = os.path.join(current_directory, 'data/test_summer')
+    winter_path = os.path.join(current_directory, 'data/test_winter')
+
+    test_1(summer_path, winter_path)
+    test_2(summer_path, winter_path)
+
+    print('summer_winter_dataset successfuly passed all tests')
